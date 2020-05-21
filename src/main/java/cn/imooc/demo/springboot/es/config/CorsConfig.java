cc544fefcffc73e0cc686d6c8851f9e8d081bdc8
huang zexin <342756877@qq.com>
2020-05-20 23:57:18 -0400

    前端跨域

---
+++ src/main/java/cn/imooc/demo/springboot/es/config/CorsConfig.java
@@ -0,0 +1,35 @@
+package cn.imooc.demo.springboot.es.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+/**
+ * @author ZEXIN HUANG
+ * @version 1.0
+ * @date 2020-05-20 23:51
+ */
+/**
+ * 解决前端跨域问题
+ *
+ * @author sixkery
+ * @date 2019/11/24
+ */
+@Configuration
+public class CorsConfig {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
